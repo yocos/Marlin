d972a6e9c28343cde6a8562df528396611e92220
yocos <ycosniam@gmail.com>
2017-10-15 15:49:18 +0200

    Fix z probe heigh

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -681,7 +681,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -30  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.47   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.37   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
